4dbe871beb6c08e4523b07ec8a99e1c08eaf6624
h.nedunuri <h.nedunuri@corrosionradar.com>
2022-04-03 23:36:36 +0100

    pandas data visualisation

--- prediction_interval_main/__init__.py
+++ prediction_interval_main/__init__.py
@@ -11,7 +11,7 @@ from sklearn.ensemble import GradientBoostingRegressor
 from pathlib import Path
 PATH_TESTS_DATA = Path(__file__).resolve().parents
 Data_path = PATH_TESTS_DATA[1] / "data"
-print(Data_path)
+
 files =[ csvfile for csvfile in Data_path.iterdir() if csvfile.is_file() and csvfile.suffix == ".csv"]
 # files = glob.glob('D:\personal\GITHUB_project_prediction_interval')
 
@@ -33,42 +33,45 @@ cf.go_offline(connected=True)
 data = pd.read_csv(files[2], parse_dates=['timestamp'], index_col='timestamp').sort_index()
 print(data.columns)
 
-# data = data.rename(columns={"energy": "actual"})
+data = data.rename(columns={"energy": "actual"})
 
-# data.columns
-# data_to_plot = data.loc["2015"].copy()
-# def plot_timescale(timescale, selection, theme):
-#     """
-#     Plot the energy consumption on different timescales (day, week, month).
+
+data_to_plot = data.loc["2015"].copy()
+def plot_timescale(timescale, selection, theme):
+    """
+    Plot the energy consumption on different timescales (day, week, month).
     
-#     :param timescale: the timescale to use
-#     :param selection: the numeric value of the timescale selection (for example the 15th day
-#     of the year or the 1st week of the year)
-#     :param theme: aesthetics of plot
-#     """
-#     # Subset based on timescale and selection
-#     subset = data_to_plot.loc[
-#         getattr(data_to_plot.index, timescale) == selection, "actual"
-#     ].copy()
+    :param timescale: the timescale to use
+    :param selection: the numeric value of the timescale selection (for example the 15th day
+    of the year or the 1st week of the year)
+    :param theme: aesthetics of plot
+    """
+    # Subset based on timescale and selection
+    subset = data_to_plot.loc[
+        getattr(data_to_plot.index, timescale) == selection, "actual"
+    ].copy()
 
-#     if subset.empty:
-#         print("Choose another selection")
-#         return
+    if subset.empty:
+        print("Choose another selection")
+        return
     
-#     # Make an interactive plot
-#     fig = subset.iplot(
-#             title=f"Energy for {selection} {timescale.title()}", theme=theme, asFigure=True
-#     )
-#     fig['layout']['height'] = 900
-#     fig['layout']['width'] = 1400
-#     iplot(fig)
+    # Make an interactive plot
+    fig = subset.iplot(
+            title=f"Energy for {selection} {timescale.title()}", theme=theme, asFigure=True
+    )
+    fig['layout']['height'] = 900
+    fig['layout']['width'] = 1400
+    iplot(fig)
 
-# _ = interact(
-#     plot_timescale,
-#     timescale=widgets.RadioButtons(
-#         options=["dayofyear", "week", "month"], value="dayofyear"
-#     ),
-#     # Selection 
-#     selection=widgets.IntSlider(value=16, min=0, max=365),
-#     theme=widgets.Select(options=cf.themes.THEMES.keys(), value='ggplot')
-# )
+_ = interact(
+    plot_timescale,
+    timescale=widgets.RadioButtons(
+        options=["dayofyear", "week", "month"], value="dayofyear"
+    ),
+    # Selection 
+    selection=widgets.IntSlider(value=16, min=0, max=365),
+    theme=widgets.Select(options=cf.themes.THEMES.keys(), value='ggplot')
+)
+
+data.loc['2015-01-01':'2015-07-01', "actual"].iplot(layout=dict(title='2015 Energy Consumption', height=800))
+data.resample('12 H')["actual"].mean().iplot(layout=dict(title='Energy Data Resampled at 12 Hours', height=800,yaxis=dict(title='kWh')))
