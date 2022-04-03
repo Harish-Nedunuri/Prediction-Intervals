45792c7c52350e609835dde8f1002428201e4f72
h.nedunuri <h.nedunuri@corrosionradar.com>
2022-04-03 22:44:07 +0100

    plotly_functions added

--- prediction_interval_main/__init__.py
+++ prediction_interval_main/__init__.py
@@ -1,173 +1,69 @@
+# Data Manipulation
 import pandas as pd
 import numpy as np
-# Plotly
-import plotly.graph_objs as go
-from plotly.offline import iplot, plot, init_notebook_mode
-init_notebook_mode(connected=True)
-# import plotly_express as px
-# from ipywidgets import interact, widgets
 
-# cufflinks is a wrapper on plotly
-# import cufflinks as cf
-# cf.go_offline(connected=True)
 
 # Modeling
 from sklearn.base import BaseEstimator
 from sklearn.ensemble import GradientBoostingRegressor
 
-import os
-from pathlib import Path
-
-# PATH_DATA=Path((__file__).resolve().parent / "data")
+# File finding
 import glob
 files = glob.glob('../data/*_energy_data.csv')
 
+# Interactivity
+from ipywidgets import interact, widgets
+
+# Visualization
+
+# Plotly
+import plotly.graph_objs as go
+from plotly.offline import iplot, plot, init_notebook_mode
+init_notebook_mode(connected=True)
+import plotly_express as px
+
+# cufflinks is a wrapper on plotly
+import cufflinks as cf
+cf.go_offline(connected=True)
+#%%
 data = pd.read_csv(files[2], parse_dates=['timestamp'], index_col='timestamp').sort_index()
 data.head()
 data = data.rename(columns={"energy": "actual"})
-
-data_to_plot = data.loc["2015"].copy()
-
-
-# Train and test sets
-X_train = data.loc["2015":"2016"].copy()
-X_test = data.loc["2017":].copy()
-y_train = X_train.pop("actual")
-y_test = X_test.pop("actual")
-
-# assert X_train.index.max() < X_test.index.min()
-
-X_train.tail()
-X_test.head()
-
-# Set lower and upper quantile
-LOWER_ALPHA = 0.15
-UPPER_ALPHA = 0.85
-
-N_ESTIMATORS = 100
-MAX_DEPTH = 5
-
-# Each model has to be separate
-
-lower_model = GradientBoostingRegressor(
-    loss="quantile", alpha=LOWER_ALPHA, n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH
-)
-# The mid model will use the default
-mid_model = GradientBoostingRegressor(loss="ls", n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH)
-
-upper_model = GradientBoostingRegressor(
-    loss="quantile", alpha=UPPER_ALPHA, n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH
-)
-
-_ = lower_model.fit(X_train, y_train)
-_ = mid_model.fit(X_train, y_train)
-_ = upper_model.fit(X_train, y_train)
 #%%
-predictions = pd.DataFrame(y_test)
-predictions['lower'] = lower_model.predict(X_test)
-predictions['mid'] = mid_model.predict(X_test)
-predictions['upper'] = upper_model.predict(X_test)
-
-assert (predictions['upper'] > predictions['lower']).all()
-
-predictions.tail()
-def plot_intervals(predictions, mid=False, start=None, stop=None, title=None):
+data.columns
+data_to_plot = data.loc["2015"].copy()
+def plot_timescale(timescale, selection, theme):
     """
-    Function for plotting prediction intervals as filled area chart.
+    Plot the energy consumption on different timescales (day, week, month).
     
-    :param predictions: dataframe of predictions with lower, upper, and actual columns (named for the target)
-    :param whether to show the mid prediction
-    :param start: optional parameter for subsetting start of predictions
-    :param stop: optional parameter for subsetting end of predictions
-    :param title: optional string title
-    
-    :return fig: plotly figure
+    :param timescale: the timescale to use
+    :param selection: the numeric value of the timescale selection (for example the 15th day
+    of the year or the 1st week of the year)
+    :param theme: aesthetics of plot
     """
-    # Subset if required
-    predictions = (
-        predictions.loc[start:stop].copy()
-        if start is not None or stop is not None
-        else predictions.copy()
-    )
-    data = []
+    # Subset based on timescale and selection
+    subset = data_to_plot.loc[
+        getattr(data_to_plot.index, timescale) == selection, "actual"
+    ].copy()
 
-    # Lower trace will fill to the upper trace
-    trace_low = go.Scatter(
-        x=predictions.index,
-        y=predictions["lower"],
-        fill="tonexty",
-        line=dict(color="darkblue"),
-        fillcolor="rgba(173, 216, 230, 0.4)",
-        showlegend=True,
-        name="lower",
-    )
-    # Upper trace has no fill
-    trace_high = go.Scatter(
-        x=predictions.index,
-        y=predictions["upper"],
-        fill=None,
-        line=dict(color="orange"),
-        showlegend=True,
-        name="upper",
-    )
-
-    # Must append high trace first so low trace fills to the high trace
-    data.append(trace_high)
-    data.append(trace_low)
+    if subset.empty:
+        print("Choose another selection")
+        return
     
-    if mid:
-        trace_mid = go.Scatter(
-        x=predictions.index,
-        y=predictions["mid"],
-        fill=None,
-        line=dict(color="green"),
-        showlegend=True,
-        name="mid",
+    # Make an interactive plot
+    fig = subset.iplot(
+            title=f"Energy for {selection} {timescale.title()}", theme=theme, asFigure=True
     )
-        data.append(trace_mid)
+    fig['layout']['height'] = 900
+    fig['layout']['width'] = 1400
+    iplot(fig)
 
-    # Trace of actual values
-    trace_actual = go.Scatter(
-        x=predictions.index,
-        y=predictions["actual"],
-        fill=None,
-        line=dict(color="black"),
-        showlegend=True,
-        name="actual",
-    )
-    data.append(trace_actual)
-
-    # Layout with some customization
-    layout = go.Layout(
-        height=900,
-        width=1400,
-        title=dict(text="Prediction Intervals" if title is None else title),
-        yaxis=dict(title=dict(text="kWh")),
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list(
-                    [
-                        dict(count=1, label="1d", step="day", stepmode="backward"),
-                        dict(count=7, label="1w", step="day", stepmode="backward"),
-                        dict(count=1, label="1m", step="month", stepmode="backward"),
-                        dict(count=1, label="YTD", step="year", stepmode="todate"),
-                        dict(count=1, label="1y", step="year", stepmode="backward"),
-                        dict(step="all"),
-                    ]
-                )
-            ),
-            rangeslider=dict(visible=True),
-            type="date",
-        ),
-    )
-
-    fig = go.Figure(data=data, layout=layout)
-
-    # Make sure font is readable
-    fig["layout"]["font"] = dict(size=20)
-    fig.layout.template = "plotly_white"
-    return fig
-
-
-# Example plot subsetted to one week
-fig = plot_intervals(predictions, start="2017-03-01", stop="2017-03-08")
+_ = interact(
+    plot_timescale,
+    timescale=widgets.RadioButtons(
+        options=["dayofyear", "week", "month"], value="dayofyear"
+    ),
+    # Selection 
+    selection=widgets.IntSlider(value=16, min=0, max=365),
+    theme=widgets.Select(options=cf.themes.THEMES.keys(), value='ggplot')
+)
