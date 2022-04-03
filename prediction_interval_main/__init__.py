98dbc231adb92f9e7a87e519425a3960db557cb4
h.nedunuri <h.nedunuri@corrosionradar.com>
2022-04-03 23:59:47 +0100

    training completed

--- prediction_interval_main/__init__.py
+++ prediction_interval_main/__init__.py
@@ -7,71 +7,60 @@ import os
 from sklearn.base import BaseEstimator
 from sklearn.ensemble import GradientBoostingRegressor
 
-# File finding
+
 from pathlib import Path
 PATH_TESTS_DATA = Path(__file__).resolve().parents
 Data_path = PATH_TESTS_DATA[1] / "data"
 
 files =[ csvfile for csvfile in Data_path.iterdir() if csvfile.is_file() and csvfile.suffix == ".csv"]
-# files = glob.glob('D:\personal\GITHUB_project_prediction_interval')
-
-# Interactivity
-from ipywidgets import interact, widgets
-
-# Visualization
-
-# Plotly
-import plotly.graph_objs as go
-from plotly.offline import iplot, plot, init_notebook_mode
-init_notebook_mode(connected=True)
-import plotly_express as px
-
-# cufflinks is a wrapper on plotly
-import cufflinks as cf
-cf.go_offline(connected=True)
-
+import matplotlib.pyplot as plt
 data = pd.read_csv(files[2], parse_dates=['timestamp'], index_col='timestamp').sort_index()
 print(data.columns)
-
 data = data.rename(columns={"energy": "actual"})
-
-
 data_to_plot = data.loc["2015"].copy()
-def plot_timescale(timescale, selection, theme):
-    """
-    Plot the energy consumption on different timescales (day, week, month).
-    
-    :param timescale: the timescale to use
-    :param selection: the numeric value of the timescale selection (for example the 15th day
-    of the year or the 1st week of the year)
-    :param theme: aesthetics of plot
-    """
-    # Subset based on timescale and selection
-    subset = data_to_plot.loc[
-        getattr(data_to_plot.index, timescale) == selection, "actual"
-    ].copy()
+plt.plot(data_to_plot)
+plt.show()
+data.loc['2015-01-01':'2015-07-01', "actual"]
+data.resample('12 H')["actual"].mean()
 
-    if subset.empty:
-        print("Choose another selection")
-        return
-    
-    # Make an interactive plot
-    fig = subset.iplot(
-            title=f"Energy for {selection} {timescale.title()}", theme=theme, asFigure=True
-    )
-    fig['layout']['height'] = 900
-    fig['layout']['width'] = 1400
-    iplot(fig)
+# Train and test sets
+X_train = data.loc["2015":"2016"].copy()
+X_test = data.loc["2017":].copy()
+y_train = X_train.pop("actual")
+y_test = X_test.pop("actual")
+X_train.tail()
+X_test.head()
+# Set lower and upper quantile
+LOWER_ALPHA = 0.15
+UPPER_ALPHA = 0.85
 
-_ = interact(
-    plot_timescale,
-    timescale=widgets.RadioButtons(
-        options=["dayofyear", "week", "month"], value="dayofyear"
-    ),
-    # Selection 
-    selection=widgets.IntSlider(value=16, min=0, max=365),
-    theme=widgets.Select(options=cf.themes.THEMES.keys(), value='ggplot')
+N_ESTIMATORS = 100
+MAX_DEPTH = 5
+
+# Each model has to be separate
+
+lower_model = GradientBoostingRegressor(
+    loss="quantile", alpha=LOWER_ALPHA, n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH
+)
+# The mid model will use the default
+mid_model = GradientBoostingRegressor(loss="ls", n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH)
+
+upper_model = GradientBoostingRegressor(
+    loss="quantile", alpha=UPPER_ALPHA, n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH
 )
 
-data.loc['2015-01-01':'2015-07-01', "actual"].iplot(layout=dict(title='2015 Energy Consumption', height=800))
-data.resample('12 H')["actual"].mean().iplot(layout=dict(title='Energy Data Resampled at 12 Hours', height=800,yaxis=dict(title='kWh')))
+_ = lower_model.fit(X_train, y_train)
+_ = mid_model.fit(X_train, y_train)
+_ = upper_model.fit(X_train, y_train)
+
+predictions = pd.DataFrame(y_test)
+predictions['lower'] = lower_model.predict(X_test)
+predictions['mid'] = mid_model.predict(X_test)
+predictions['upper'] = upper_model.predict(X_test)
+
+assert (predictions['upper'] > predictions['lower']).all()
+
+plt.figure()
+plt.plot(predictions['lower'])
+plt.plot(predictions['mod'])
+plt.show()
