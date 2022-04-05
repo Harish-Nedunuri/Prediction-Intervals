d6d09b7d723f0b5fad3d3aa2eaf8113a9f4cd747
h.nedunuri <h.nedunuri@corrosionradar.com>
2022-04-05 22:13:51 +0100

    pyspark data wrangling

--- prediction_interval_main/__init__.py
+++ prediction_interval_main/__init__.py
@@ -2,7 +2,9 @@
 import pandas as pd
 import numpy as np
 import os
+import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
 # Modeling
 from sklearn.base import BaseEstimator
@@ -22,15 +24,53 @@ spark = SparkSession \
     .builder \
     .appName("how to read csv file") \
     .getOrCreate()
-df_data = spark.read.csv(files[2])
-data = pd.read_csv(files[2], parse_dates=['timestamp'],index_col=["timestamp"]).sort_index()
-#%%
-data = data.rename(columns={"energy": "actual"})
-data_years = pd.DatetimeIndex(data.index).year.unique()
-data_to_plot = data.loc[f"{data_years[0]}"]
+df_spark_data= spark.read.csv(str(files[2]),header="true")
 
-def _plot_pie_chart_yearwise(data_to_plot,data_years):
-    plt.pie(data)
+df_spark_data = df_spark_data.withColumnRenamed("energy","actual")
+
+
+#data visualisation
+
+#print schema of the data frame
+df_spark_data.printSchema()
+
+#print first few rows of the data frame
+df_spark_data.show(10)
+
+# change schema of the df
+#change the timestamp str dtype to datetime dtype
+df_schema_changed = df_spark_data.withColumn("timestamp",to_timestamp("timestamp"))
+
+#change the actual column to float dtype
+def convert_schema_col_to_double(dframe, col_name):
+    df_schema_changed=dframe.withColumn(col_name,col(col_name).cast('double'))
+    return df_schema_changed
+
+def convert_schema_col_to_int(dframe, col_name):
+    df_schema_changed=dframe.withColumn(col_name,col(col_name).cast('int'))
+    return df_schema_changed
+
+df_schema_changed=convert_schema_col_to_double(df_schema_changed, "actual")
+df_schema_changed=convert_schema_col_to_double(df_schema_changed, "temperature")
+df_schema_changed=convert_schema_col_to_double(df_schema_changed, "irradiance")
+df_schema_changed=convert_schema_col_to_double(df_schema_changed, "relative_humidity")
+df_schema_changed=convert_schema_col_to_double(df_schema_changed, "time_of_day")
+df_schema_changed = convert_schema_col_to_int(df_schema_changed,"business_day")
+df_schema_changed = convert_schema_col_to_int(df_schema_changed,"day_of_week")
+df_schema_changed = convert_schema_col_to_int(df_schema_changed,"day_of_year")
+
+
+df_schema_changed.printSchema()
+
+
+#task 1
+# 1d line plot of column-actual
+#task 2
+# a pie chart of the data for unique years
+
+
+# def _plot_pie_chart_yearwise(data_to_plot,data_years):
+#     plt.pie(data)
     
 def _plot_timeseris_data(data_to_plot,label):
     plt.figure()
@@ -40,50 +80,4 @@ def _plot_timeseris_data(data_to_plot,label):
     plt.ylabel("Actual Energy")
     
     plt.show()
-_plot_timeseris_data(data_to_plot,label=f"{data_years[0]}")
-#%%
-plt.show()
-data.loc['2015-01-01':'2015-07-01', "actual"]
-data.resample('12 H')["actual"].mean()
 
-# Train and test sets
-X_train = data.loc["2015":"2016"].copy()
-X_test = data.loc["2017":].copy()
-y_train = X_train.pop("actual")
-y_test = X_test.pop("actual")
-X_train.tail()
-X_test.head()
-# Set lower and upper quantile
-LOWER_ALPHA = 0.15
-UPPER_ALPHA = 0.85
-
-N_ESTIMATORS = 100
-MAX_DEPTH = 5
-
-# Each model has to be separate
-
-lower_model = GradientBoostingRegressor(
-    loss="quantile", alpha=LOWER_ALPHA, n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH
-)
-# The mid model will use the default
-mid_model = GradientBoostingRegressor(loss="ls", n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH)
-
-upper_model = GradientBoostingRegressor(
-    loss="quantile", alpha=UPPER_ALPHA, n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH
-)
-
-_ = lower_model.fit(X_train, y_train)
-_ = mid_model.fit(X_train, y_train)
-_ = upper_model.fit(X_train, y_train)
-
-predictions = pd.DataFrame(y_test)
-predictions['lower'] = lower_model.predict(X_test)
-predictions['mid'] = mid_model.predict(X_test)
-predictions['upper'] = upper_model.predict(X_test)
-
-assert (predictions['upper'] > predictions['lower']).all()
-
-plt.figure()
-plt.plot(predictions['lower'])
-plt.plot(predictions['mod'])
-plt.show()
