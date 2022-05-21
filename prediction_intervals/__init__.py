a93ec72d7f54cc6e73c33e6280e1eb716b6d4c0c
hnedunuri <h.nedunuri@corrosionradar.com>
2022-05-21 09:42:38 +0100

    data cleaning

--- prediction_intervals/__init__.py
+++ prediction_intervals/__init__.py
@@ -6,18 +6,24 @@ import os
 
 from sklearn.base import BaseEstimator
 from sklearn.ensemble import GradientBoostingRegressor
-
+from pyspark.sql.functions import *
 
 from pathlib import Path
 
 def example_function():
     return 1 + 1
-
-def get_intervals(data, model_configs):
-    data = data.withColumn('timestamp', to_date('timestamp'))
+def _data_cleaning(data):
+    # convert the timestamp column to timestamp and sort the timestamp column
+    data = data.withColumn('timestamp',to_timestamp('timestamp'))
     data=data.sort('timestamp')
-    data=data.withColumnRenamed("energy","actual")
+    # rename the energy column
+    output_data=data.withColumnRenamed("energy","actual")
+    data_2015=data.where(data.timestamp.Year == "2015")
+
+    return output_data
+def get_intervals(data, model_configs):
     
+    cleaned_data = _data_cleaning(data)
     prediction_intervals={}
     outputs ={}
     return prediction_intervals,outputs
